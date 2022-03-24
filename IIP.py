fix IIP calculation

IIP.py
@@ -1,51 +1,37 @@
-#!/usr/bin/env python
-# coding: utf-8
 import numpy as np
-import math
+from numpy import tan,arcsin,arctan2,sqrt
 
-
-# FAAの資料を元にしているが地球の各定数はOpenTsiolkovskyに合わせている
-def posECEF_IIP_FAA(posECEF_, velECEF_, n_iter=5):
-    
-    PI = 3.1415926535898
-    A = 6378137.0
-    ONE_F = 298.257223563
-    B = (A*(1.0 - 1.0/ONE_F))
-    E2 = ((1.0/ONE_F)*(2-(1.0/ONE_F)))
-    ED2 = (E2*A*A/(B*B))
-    G = 9.80665
-
-
-    
-    a = A
-    mu = GM
-    f = 1.0 / ONE_F
-    omega_earth = OMEGA
+def posLLH_IIP_FAA(posECEF_, velECEF_, n_iter=5):
+    a = 6378137
+    mu = 3.986004418e14
+    f = 1.0 / 298.257223563
+    omega_earth = 7.292115e-5
     b = a * (1.0 - f)
     e2 = 2.0 * f - f * f
     ed2 = e2 / (1.0 - e2)
+    omegaVec = np.array([0.0, 0.0, omega_earth])
     
-    # (v)-(A): The distance frome the center of the Earth ellipsoid to the launch point (the initial approximation of r_k1, k=1)
-    r_k1 = np.linalg.norm(c.ecef_origin) #sqrt(E0**2 + F0**2 + G0**2)
+    # (v)-(A): The distance from the center of the Earth ellipsoid to the launch point (the initial approximation of r_k1, k=1)
+    r_k1 = b
    
     # (v)-(B): The radial distance from the geocenter to the launch vehicle position
-    posECI_init_ = ct.posECIfromECEF(posECEF_, second=0)
+    posECI_init_ = posECEF_
     r0 = np.linalg.norm(posECI_init_)
     if r0 < r_k1: # then tha launch vehicle position is below the Earth's surface and an impact point cannot be computed
         return np.full(3, np.nan)      # no solution
     
-    # (v)-(C): The inertial velocity compoents
-    velECI_init_ = ct.velECIfromECEF(posECEF_, velECEF_, second=0)
+    # (v)-(C): The inertial velocity components
+    velECI_init_ = velECEF_ + np.cross(omegaVec, posECEF_)
     # (v)-(D): The magnitude of the inertial velocity vector
     v0 = np.linalg.norm(velECI_init_)
     
     # (v)-(E): The eccentricity of the trajectory ellipse multiplied by the cosine of the eccentric anomaly at epoch
     eps_cos = (r0 * v0**2 / mu) - 1
-    
+    if eps_cos >= 1: # then the trajectory orbit is not elliptical, but is hyperbolic or parabolic, and an impact point cannot be computed
+        return np.full(3, np.nan)      # no solution
+
     # (v)-(F): The semi-major axis of the trajectory ellipse
     a_t = r0 / (1 - eps_cos)
-    if a_t <= 0: # then the trajectory orbit is not elliptical, but is hyperbolic or parabolic, and an impact point cannot be computed
-        return np.full(3, np.nan)      # no solution
     
     # (v)-(G): The eccentricity of the trajectory ellipse multiplied by the sine of the eccentric anomaly at epoch
     eps_sin = np.inner(posECI_init_, velECI_init_) / sqrt(mu * a_t)
@@ -102,9 +88,6 @@ def posECEF_IIP_FAA(posECEF_, velECEF_, n_iter=5):
     # (v)-(U): The geodetic latitude at impact
     phi_impact = arctan2(tan(phi_impact_tmp), 1 - e2)
     # (v)-(V): The East longitude at impact
-    lammda_impact = arctan2(Fk, Ek) - omega_earth*time_sec
+    lambda_impact = arctan2(Fk, Ek) - omega_earth*time_sec
     
-    # finish: convert to posECEF_IIP_
-    posECEF_IIP_ = coord_utils.CoordUtils([rad2deg(phi_impact), rad2deg(lammda_impact), 0]).ecef_origin
-    
-    return posECEF_IIP_
+    return np.array([phi_impact, lambda_impact, 0.0]) * 180.0 / np.pi