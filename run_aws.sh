cd1f8831dc6b447e66bfb2f513f5e3d26100053d
A-Hayasaka <akira.hayasaka@istellartech.com>
2022-04-01 15:33:56 +0900

    update shell script

--- run_aws.sh
+++ run_aws.sh
@@ -9,11 +9,12 @@ fi
 echo $1
 aws s3 cp s3://ist-system/Trajectory_Optimization/$1 . --recursive --exclude "output*"
 
+mkdir output
 ls *.json | xargs python3 Trajectory_Optimization.py
 
 aws s3 cp output s3://ist-system/Trajectory_Optimization/$1/output --recursive
 
 rm -r output
-rm ./*.json
-rm ./*.csv
+rm *.json
+rm *.csv
 cp _user_constraints_empty.py user_constraints.py
